0b6a9be80fbd0c4f2799bc10d8d02a730133b9a1
Tomasz Wróbel <tomekwr@gmail.com>
2026-04-09 13:33:33 +0200

    update

--- nvim/lua/init.lua
+++ nvim/lua/init.lua
@@ -36,6 +36,66 @@ vim.keymap.set("n", "<leader>nf", function() vim.cmd.edit(vim.fn.fnameescape(lru
 
 vim.cmd([[
 
+  let $BASH_ENV = "$HOME/.zsh_aliases"
+  let mapleader = " "
+
+  set background=dark
+  set tabstop=2
+  set softtabstop=2
+  set shiftwidth=2
+  set expandtab
+  set nowrap
+  set ignorecase              " Case insensitive searching
+  set smartcase               " But become case sensitive if you type uppercase characters
+  set hls
+  set autowriteall
+  set splitbelow
+  set splitright
+  set confirm
+  set nofixendofline
+  set noshowmode              " Don't show -- INSERT -- because it's already there in upper status line
+  set clipboard=unnamed       " To use system clipboard
+  set updatetime=100          " To make gitgutter refresh quicker
+  set signcolumn=yes          " Always show sign column
+  set lazyredraw
+  set iskeyword+=-
+  set hidden                  " To allow terminal buffers update in bg
+  set foldenable              " Enable folding
+  set foldlevelstart=10       " Open most folds by default
+  set foldnestmax=10          " 10 nested fold max
+  set foldmethod=indent       " Fold based on indent level
+  set mouse=a                 " Enable vim mouse scrolling while in tmux
+  set nomodeline
+
+  let g:ruby_indent_block_style = 'do'
+  let g:ruby_indent_assignment_style = 'variable'
+
+  let g:lightline = {
+    \ 'active': {
+    \   'left': [
+    \     [ 'mode', 'paste' ],
+    \     [ 'readonly', 'relativepath', 'modified' ]
+    \   ],
+    \   'right': [
+    \     [ 'lineinfo' ],
+    \     [ 'percent' ],
+    \     []
+    \   ]
+    \ },
+    \ }
+
+
+  let g:gitgutter_override_sign_column_highlight = 0
+  "colorscheme vim
+  colorscheme solarized
+  set notermguicolors
+
+  hi Search ctermfg=NONE ctermbg=NONE cterm=underline
+  hi IncSearch ctermfg=NONE ctermbg=NONE cterm=underline
+  hi MatchParen ctermbg=NONE ctermfg=green cterm=NONE
+  hi VertSplit ctermbg=0 ctermfg=0
+  hi SignColumn ctermbg=bg
+
   command! -nargs=+ GrepRaw cex system('ag --column --hidden --ignore .git --ignore \*.rbi --ignore \*.log <args>')
   command! -nargs=+ GrepRuby cex system('ag --column --ruby <args>')
   command! -nargs=+ G GrepRaw <args>
