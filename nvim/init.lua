1bee662ce93310fb905cc072348f52fa76406232
Tomasz Wróbel <tomekwr@gmail.com>
2026-04-17 15:19:35 +0200

    rm vim plug

--- nvim/init.lua
+++ nvim/init.lua
@@ -32,38 +32,6 @@ require('mini.move').setup()
 vim.opt.rtp:append("/opt/homebrew/opt/fzf")
 
 vim.cmd([[
-  "set rtp+=/opt/homebrew/opt/fzf
-
-  call plug#begin()
-  " Plug 'augmentcode/augment.vim'
-  "Plug '/usr/local/opt/fzf'
-  "Plug 'tpope/vim-sensible'
-  "Plug 'tpope/vim-commentary'
-  "Plug 'tpope/vim-endwise'
-  "Plug 'tpope/vim-fugitive'
-  "Plug 'tpope/vim-rhubarb'
-  "Plug 'tpope/vim-surround'
-  "Plug 'tpope/vim-repeat'
-  "Plug 'tpope/vim-rsi'
-  "Plug 'tpope/vim-eunuch'
-  "Plug 'tpope/vim-vinegar'
-  "Plug 'vim-ruby/vim-ruby'
-  "Plug 'itchyny/lightline.vim'
-  "Plug 'terryma/vim-multiple-cursors'
-  "Plug 'michaeljsmith/vim-indent-object'
-  "Plug 'airblade/vim-gitgutter'
-  "Plug 'altercation/vim-colors-solarized'
-  "Plug 'kana/vim-textobj-user'
-  "Plug 'kana/vim-textobj-entire'
-  "Plug 'beloglazov/vim-textobj-quotes'
-  "Plug 'bkad/camelcasemotion'
-  "Plug 'svermeulen/vim-easyclip'
-  "Plug 'AndrewRadev/splitjoin.vim'
-  "Plug 'iamcco/markdown-preview.nvim', { 'do': 'cd app && npx --yes yarn install' }
-  call plug#end()
-]])
-
-vim.cmd([[
   set background=dark
   set tabstop=2
   set softtabstop=2
